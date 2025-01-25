81e29f445cc77b0e810bb342f4da1a3446e80b35
CodingKarp <alexbishkek67@gmail.com>
2025-01-25 12:25:47 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,3 +2,18 @@
 select COUNT(*) as customers_count
 from customers;
 
+--находит 10 лучших продавцов за весь период--
+select
+    emp.first_name || ' ' || emp.last_name as seller,
+    COUNT(sa.sales_person_id) as operations,
+    FLOOR(SUM(p.price * sa.quantity)) as income
+from sales as sa
+inner join employees as emp
+    on sa.sales_person_id = emp.employee_id
+inner join products as p
+    on sa.product_id = p.product_id
+group by seller
+order by FLOOR(SUM(p.price * sa.quantity)) desc limit 10;
+
+
+
